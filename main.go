d77e067c685b86b407f5daaf82bdde7c8444ae3b
JackWong <jwismaster@gmail.com>
2023-05-14 15:58:06 +0800

    refactor: refactor code for better structure and readability
    - Add check for callback query from inline keyboard
    - Add check for restart command and specified user ID
    - Add message recording for non-specified users
    - Add inline keyboard for VM selection
    - Fix typo in welcome message
    - Rearrange code for better structure and readability
    
    Signed-off-by: JackWong <jwismaster@gmail.com>

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"rs/esxi"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
 )
+
+var messageMap = make(map[int64]MessageInfo)
 
 func main() {
 	// Load environment variables from .env file
@@ -50,10 +53,30 @@ func main() {
 	// Handle incoming updates
 	for update := range updates {
 		if update.Message == nil {
+			// Check if update is a callback query from Inline Keyboard
+			if update.CallbackQuery != nil {
+				handleInlineKeyboard(update.CallbackQuery, bot)
+			}
 			continue
 		}
+
+		// 增加判断，如果用户发送的是 `/restart` 命令且是您指定的用户，则发送一条新的消息
+		if update.Message.Command() == "restart" && update.Message.From.ID == 917774935 {
+			// 发送一条新的消息
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "请在命令中指定要重启的虚拟机，例如：/restart vm_name")
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+			continue
+		}
+
+		// 如果用户不是指定的用户，则记录消息信息并不做处理
 		if update.Message.From.ID != 917774935 {
-			go send("操作失败!", update, bot)
+			messageInfo := MessageInfo{
+				ChatID:     update.Message.Chat.ID,
+				MessageID:  update.Message.MessageID,
+				CreateTime: update.Message.Date,
+			}
+			messageMap[update.Message.Chat.ID] = messageInfo
 			continue
 		}
 
@@ -62,6 +85,9 @@ func main() {
 			continue
 		}
 		switch update.Message.Command() {
+		case "start":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "欢迎使用 ESXi Bot，您可以使用以下命令：\n/list - 获取当前 ESXi 主机上的虚拟机列表\n/restart - 重启指定虚拟机")
+			bot.Send(msg)
 		case "list":
 			m, _ := send(fmt.Sprintf("获取中, 请稍等..."), update, bot)
 			lists, err := getVMLists()
@@ -81,20 +107,42 @@ func main() {
 				}
 				listStr += fmt.Sprintf("%-12s %s\n", list.Config.Name, state)
 			}
-			go edit(m.Chat.ID, m.MessageID, fmt.Sprintf("获取成功: \n%s", listStr), bot)
+			replyMsg := fmt.Sprintf("获取成功: \n%s", listStr)
+			// Create Inline Keyboard
+			inlineKeyboard := createInlineKeyboard(lists)
+			if len(inlineKeyboard.InlineKeyboard) > 0 {
+				replyMsg += "\n请选择要重启的虚拟机："
+				inlineKeyboardMessage := tgbotapi.NewMessage(update.Message.Chat.ID, replyMsg)
+				inlineKeyboardMessage.ReplyMarkup = &inlineKeyboard
+				bot.Send(inlineKeyboardMessage)
+			} else {
+				go edit(m.Chat.ID, m.MessageID, replyMsg, bot)
+			}
 			break
 		case "restart":
+			args := update.Message.CommandArguments()
+			if args == "" {
+				go send("请输入要重启的虚拟机名称，例如：/restart vm_name", update, bot)
+				break
+			}
 			m, _ := send(fmt.Sprintf("重启中, 请稍等..."), update, bot)
-			err := restartVM(update.Message.CommandArguments())
+			err := restartVM(args)
 			if err != nil {
 				go edit(m.Chat.ID, m.MessageID, fmt.Sprintf("操作失败, 原因:%s", err.Error()), bot)
 				break
 			}
-			go edit(m.Chat.ID, m.MessageID, fmt.Sprintf("操作成功"), bot)
+			go edit(m.Chat.ID, m.MessageID, fmt.Sprintf("%s 已重启", args), bot)
+			break
 		default:
 			//...
 		}
 	}
+}
+
+type MessageInfo struct {
+	ChatID     int64
+	MessageID  int
+	CreateTime int
 }
 
 func send(msg string, update tgbotapi.Update, bot *tgbotapi.BotAPI) (tgbotapi.Message, error) {
@@ -102,6 +150,7 @@ func send(msg string, update tgbotapi.Update, bot *tgbotapi.BotAPI) (tgbotapi.Me
 	m.ReplyToMessageID = update.Message.MessageID
 	return bot.Send(m)
 }
+
 func edit(chatID int64, messageID int, msg string, bot *tgbotapi.BotAPI) {
 	m := tgbotapi.NewEditMessageText(chatID, messageID, msg)
 	bot.Send(m)
@@ -133,4 +182,46 @@ func restartVM(name string) error {
 		return err
 	}
 	return nil
+}
+
+func createInlineKeyboard(vmList []mo.VirtualMachine) tgbotapi.InlineKeyboardMarkup {
+	var buttons []tgbotapi.InlineKeyboardButton
+	var rows [][]tgbotapi.InlineKeyboardButton
+	for i, vm := range vmList {
+		buttonText := fmt.Sprintf("%s", vm.Config.Name)
+		callbackData := fmt.Sprintf("restart_%s", vm.Config.Name)
+		button := tgbotapi.NewInlineKeyboardButtonData(buttonText, callbackData)
+		buttons = append(buttons, button)
+		if (i+1)%5 == 0 || i == len(vmList)-1 {
+			rows = append(rows, buttons)
+			buttons = []tgbotapi.InlineKeyboardButton{}
+		}
+	}
+	if len(rows) == 0 {
+		return tgbotapi.InlineKeyboardMarkup{}
+	}
+	inlineKeyboard := tgbotapi.NewInlineKeyboardMarkup(rows...)
+	return inlineKeyboard
+}
+
+func handleInlineKeyboard(callbackQuery *tgbotapi.CallbackQuery, bot *tgbotapi.BotAPI) {
+	if strings.HasPrefix(callbackQuery.Data, "restart_") {
+		vmName := strings.TrimPrefix(callbackQuery.Data, "restart_")
+		err := restartVM(vmName)
+		if err != nil {
+			errMsg := fmt.Sprintf("操作失败, 原因:%s", err.Error())
+			answerCallbackQuery := tgbotapi.NewCallback(callbackQuery.ID, errMsg)
+			bot.AnswerCallbackQuery(answerCallbackQuery)
+			go sendNewMessage(callbackQuery.Message.Chat.ID, errMsg, bot)
+			return
+		}
+		replyMsg := fmt.Sprintf("%s 已重启", vmName)
+		answerCallbackQuery := tgbotapi.NewCallback(callbackQuery.ID, replyMsg)
+		bot.AnswerCallbackQuery(answerCallbackQuery)
+		go sendNewMessage(callbackQuery.Message.Chat.ID, replyMsg, bot)
+	}
+}
+func sendNewMessage(chatID int64, msg string, bot *tgbotapi.BotAPI) {
+	m := tgbotapi.NewMessage(chatID, msg)
+	bot.Send(m)
 }
